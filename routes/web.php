17a2e32c05aface26902b668379b6aeae022ccf1
Muhammad Ismail <mrprogamer1909@gmail.com>
2023-05-29 14:05:03 +0500

    routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/', function () {
+    return view('signup');
+});
+
+
